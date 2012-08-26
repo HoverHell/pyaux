25e47351980157c9d2b4eaa0ef28bd46867e43c3
HoverHell <hoverhell@gmail.com>
2012-08-26 15:13:34 +0400

    minor stylistic change

--- pyaux/exc_ipdb.py
+++ pyaux/exc_ipdb.py
@@ -13,16 +13,16 @@ import sys
 
 def info(type, value, tb):
    if hasattr(sys, 'ps1') or not sys.stderr.isatty():
-      # we are in interactive mode or we don't have a tty-like
-      # device, so we call the default hook
-      sys.__excepthook__(type, value, tb)
+       # we are in interactive mode or we don't have a tty-like
+       # device, so we call the default hook
+       sys.__excepthook__(type, value, tb)
    else:
-      import traceback, ipdb
-      # we are NOT in interactive mode, print the exception...
-      traceback.print_exception(type, value, tb)
-      print
-      # ...then start the debugger in post-mortem mode.
-      ipdb.pm()
+       import traceback, ipdb
+       # we are NOT in interactive mode, print the exception...
+       traceback.print_exception(type, value, tb)
+       print
+       # ...then start the debugger in post-mortem mode.
+       ipdb.pm()
 
 
 def init():
