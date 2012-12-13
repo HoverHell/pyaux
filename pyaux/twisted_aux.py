f38a7f64b079bc49f648206976f5788481c3f081
HoverHell <hoverhell@gmail.com>
2012-12-13 19:00:24 +0400

    twisted_log fix

--- pyaux/twisted_aux.py
+++ pyaux/twisted_aux.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf8
 
+import sys
 import twisted.python.log as twisted_log
 from . import exc_log
 
@@ -9,14 +10,16 @@ class ExcLogObserver(object):
     """ A Twisted Log Observer that uses advanced logging for exceptions """
     ## See twisted.python.log.PythonLoggingObserver for information on what is what
     def emit(self, eventDict):
-        if not eventDict['isError']:
+        if not eventDict.get('isError'):
             return  # not for us
-        #e_t, e_v, e_tb = sys.exc_info()
-        ## More correct:
-        vf = eventDict['failure']
-        e_t, e_v, e_tb = vf.type, vf.value, vf.getTracebackObject()
-        #sys.excepthook(e_t, e_v, e_tb)
-        exc_log.advanced_info_safe(e_t, e_v, e_tb)
+        ## Obtain the info:
+        vf = eventDict.get('failure')
+        if vf is not None:
+            e_t, e_v, e_tb = vf.type, vf.value, vf.getTracebackObject()
+        else:
+            e_t, e_v, e_tb = sys.exc_info()
+        #sys.excepthook(e_t, e_v, e_tb)  # default-hook
+        exc_log.advanced_info_safe(e_t, e_v, e_tb)  # exc_log
     def start(self):
         twisted_log.addObserver(self.emit)
     def stop(self):
