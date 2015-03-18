c32fddfb5c31c03a6f0a59d9a0bf018fb9f8ecb9
HoverHell <hoverhell@gmail.com>
2015-03-18 18:17:45 +0300

    Release 1.4.0

--- pyaux/__init__.py
+++ pyaux/__init__.py
@@ -1,7 +1,7 @@
 # coding: utf8
 """ A collection of useful helpers """
 
-__version__ = "1.3.2"
+__version__ = "1.4.0"
 
 ## NOTE: no modules imported here should import `decimal` (otherwise
 ##   `use_cdecimal` might become problematic for them)
