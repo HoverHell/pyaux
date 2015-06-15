849e851e799533450ff7d0547879f77620b066fe
HoverHell <hoverhell@gmail.com>
2015-06-15 13:38:09 +0300

    fix fjson.py

--- pyaux/bin/fjson.py
+++ pyaux/bin/fjson.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # coding: utf8
 
+import os
 import json
 import sys
+
+from pyaux.base import to_bytes
 
 
 def main():
@@ -17,7 +20,7 @@ def main():
         return 13
     data_out = json.dumps(
         data, indent=indent, sort_keys=True, ensure_ascii=False)
-    sys.stdout.write(data_out)
+    sys.stdout.write(to_bytes(data_out))
     sys.stdout.write("\n")
 
 
