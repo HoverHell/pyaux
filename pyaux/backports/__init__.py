ed9b26eb200778fab6352f81f257ff3ea792ce15
HoverHell <hoverhell@gmail.com>
2016-07-04 14:00:02 +0300

    sh_quote_prettier

--- pyaux/backports/__init__.py
+++ pyaux/backports/__init__.py
@@ -17,3 +17,28 @@ def sh_quote(s):
     # use single quotes, and put single quotes into double quotes
     # the string $'b is then quoted as '$'"'"'b'
     return "'" + s.replace("'", "'\"'\"'") + "'"
+
+
+def sh_quote_prettier(s):
+    r"""
+    Quote a value for copypasteability in a posix commandline.
+
+    A more readable version than the backported one.
+
+    >>> sh_quote_prettier("'one's one'")
+    "\\''one'\\''s one'\\'"
+    """
+    if not s:
+        return "''"
+    if _sh_find_unsafe(s) is None:
+        return s
+
+    # A shorter version: backslash-escaped single quote.
+    result = "'" + s.replace("'", "'\\''") + "'"
+    # Cleanup the empty excesses at the ends
+    _overedge = "''"
+    if result.startswith(_overedge):
+        result = result[len(_overedge):]
+    if result.endswith(_overedge):
+        result = result[:-len(_overedge)]
+    return result
