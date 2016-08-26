474a627dcd159610e8d3f2f5caabeaf083476c5b
HoverHell <hoverhell@gmail.com>
2016-08-26 13:33:21 +0300

    MVOD.getlist, MultiValueDict from django

--- pyaux/dicts.py
+++ pyaux/dicts.py
@@ -85,6 +85,7 @@ remvdodd('mod': ..., 'u1': 'y', 1: 3, 5: 2)
 # ipy to doctest:  sed -r 's/^In[^:]+: />>> /; s/^Out[^:]+: //; /^ *$/ d'
 # test to locals:  from pyaux import dicts; reload(dicts); from pyaux.dicts import *; exec '\n'.join(v[4:] for v in dicts.__doc__.split('\n\n')[-1].splitlines() if v.startswith('>>> '))
 
+import six
 import copy
 import itertools
 try:
@@ -259,6 +260,202 @@ class OrderedDict(dict, ODReprMixin, DictMixin):
 
     def __ne__(self, other):
         return not self.__eq__(other)  # not self == other
+
+
+# ###
+# MultiValueDict from django.utils.datastructures
+# ###
+
+class MultiValueDictKeyError(KeyError):
+    pass
+
+
+class MultiValueDict(dict):
+    """
+    A subclass of dictionary customized to handle multiple values for the
+    same key.
+
+    >>> d = MultiValueDict({'name': ['Adrian', 'Simon'], 'position': ['Developer']})
+    >>> d['name']
+    'Simon'
+    >>> d.getlist('name')
+    ['Adrian', 'Simon']
+    >>> d.getlist('doesnotexist')
+    []
+    >>> d.getlist('doesnotexist', ['Adrian', 'Simon'])
+    ['Adrian', 'Simon']
+    >>> d.get('lastname', 'nonexistent')
+    'nonexistent'
+    >>> d.setlist('lastname', ['Holovaty', 'Willison'])
+
+    This class exists to solve the irritating problem raised by cgi.parse_qs,
+    which returns a list for every key, even though most Web forms submit
+    single name-value pairs.
+    """
+    def __init__(self, key_to_list_mapping=()):
+        super(MultiValueDict, self).__init__(key_to_list_mapping)
+
+    def __repr__(self):
+        return "<%s: %s>" % (self.__class__.__name__,
+                             super(MultiValueDict, self).__repr__())
+
+    def __getitem__(self, key):
+        """
+        Returns the last data value for this key, or [] if it's an empty list;
+        raises KeyError if not found.
+        """
+        try:
+            list_ = super(MultiValueDict, self).__getitem__(key)
+        except KeyError:
+            raise MultiValueDictKeyError(repr(key))
+        try:
+            return list_[-1]
+        except IndexError:
+            return []
+
+    def __setitem__(self, key, value):
+        super(MultiValueDict, self).__setitem__(key, [value])
+
+    def __copy__(self):
+        return self.__class__([
+            (k, v[:])
+            for k, v in self.lists()
+        ])
+
+    def __deepcopy__(self, memo=None):
+        if memo is None:
+            memo = {}
+        result = self.__class__()
+        memo[id(self)] = result
+        for key, value in dict.items(self):
+            dict.__setitem__(result, copy.deepcopy(key, memo),
+                             copy.deepcopy(value, memo))
+        return result
+
+    def __getstate__(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['_data'] = {k: self.getlist(k) for k in self}
+        return obj_dict
+
+    def __setstate__(self, obj_dict):
+        data = obj_dict.pop('_data', {})
+        for k, v in data.items():
+            self.setlist(k, v)
+        self.__dict__.update(obj_dict)
+
+    def get(self, key, default=None):
+        """
+        Returns the last data value for the passed key. If key doesn't exist
+        or value is an empty list, then default is returned.
+        """
+        try:
+            val = self[key]
+        except KeyError:
+            return default
+        if val == []:
+            return default
+        return val
+
+    def getlist(self, key, default=None):
+        """
+        Returns the list of values for the passed key. If key doesn't exist,
+        then a default value is returned.
+        """
+        try:
+            return super(MultiValueDict, self).__getitem__(key)
+        except KeyError:
+            if default is None:
+                return []
+            return default
+
+    def setlist(self, key, list_):
+        super(MultiValueDict, self).__setitem__(key, list_)
+
+    def setdefault(self, key, default=None):
+        if key not in self:
+            self[key] = default
+            # Do not return default here because __setitem__() may store
+            # another value -- QueryDict.__setitem__() does. Look it up.
+        return self[key]
+
+    def setlistdefault(self, key, default_list=None):
+        if key not in self:
+            if default_list is None:
+                default_list = []
+            self.setlist(key, default_list)
+            # Do not return default_list here because setlist() may store
+            # another value -- QueryDict.setlist() does. Look it up.
+        return self.getlist(key)
+
+    def appendlist(self, key, value):
+        """Appends an item to the internal list associated with key."""
+        self.setlistdefault(key).append(value)
+
+    def _iteritems(self):
+        """
+        Yields (key, value) pairs, where value is the last item in the list
+        associated with the key.
+        """
+        for key in self:
+            yield key, self[key]
+
+    def _iterlists(self):
+        """Yields (key, list) pairs."""
+        return six.iteritems(super(MultiValueDict, self))
+
+    def _itervalues(self):
+        """Yield the last value on every key list."""
+        for key in self:
+            yield self[key]
+
+    if six.PY3:
+        items = _iteritems
+        lists = _iterlists
+        values = _itervalues
+    else:
+        iteritems = _iteritems
+        iterlists = _iterlists
+        itervalues = _itervalues
+
+        def items(self):
+            return list(self.iteritems())
+
+        def lists(self):
+            return list(self.iterlists())
+
+        def values(self):
+            return list(self.itervalues())
+
+    def copy(self):
+        """Returns a shallow copy of this object."""
+        return copy.copy(self)
+
+    def update(self, *args, **kwargs):
+        """
+        update() extends rather than replaces existing key lists.
+        Also accepts keyword args.
+        """
+        if len(args) > 1:
+            raise TypeError("update expected at most 1 arguments, got %d" % len(args))
+        if args:
+            other_dict = args[0]
+            if isinstance(other_dict, MultiValueDict):
+                for key, value_list in other_dict.lists():
+                    self.setlistdefault(key).extend(value_list)
+            else:
+                try:
+                    for key, value in other_dict.items():
+                        self.setlistdefault(key).append(value)
+                except TypeError:
+                    raise ValueError("MultiValueDict.update() takes either a MultiValueDict or dictionary")
+        for key, value in six.iteritems(kwargs):
+            self.setlistdefault(key).append(value)
+
+    def dict(self):
+        """
+        Returns current object as a dict with singular values.
+        """
+        return {key: self[key] for key in self}
 
 
 # ######
@@ -448,6 +645,14 @@ class MVOD(ODReprMixin, dict):
                 lists[key].append(val)
         for key, lst in order:
             yield key, lst  # lists[k]
+
+    def getlist(self, key, default=None):
+        values = [item_val for item_key, item_val in self.iteritems() if item_key == key]
+        if values:
+            return values
+        if default is None:
+            return []
+        return default
 
     def iterkeys(self):
         for key, _ in self.iteritems():
