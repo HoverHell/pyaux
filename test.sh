2f3d5e0adc2996d8aeeb9aad6bda6982e8aee87f
HoverHell <hoverhell@gmail.com>
2017-03-23 12:55:06 +0300

    tox tests

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#######
-## Script to run all the known used testing for this library
-#######
-
-# python -m doctest pyaux/dicts.py pyaux/base.py pyaux/interpolate.py pyaux/ranges.py "$@"
-py.test --doctest-modules "$@"
