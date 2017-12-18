ac082b34b36745f5f0e50fb4ed67372a18113b31
HoverHell <hoverhell@gmail.com>
2017-12-18 18:53:08 +0300

    fmsgp_json: support multiple items

--- pyaux/bin/fmsgp_json.py
+++ pyaux/bin/fmsgp_json.py
@@ -10,17 +10,25 @@ from pyaux.base import to_bytes
 def main():
     import msgpack
     indent = int(os.environ.get('INDENT') or '2')
-    data_in = sys.stdin.read()
     try:
-        data = msgpack.loads(data_in)
+        data_in = sys.stdin.buffer
+    except AttributeError:
+        data_in = sys.stdin
+
+    try:
+        stream = msgpack.Unpacker(data_in)  # , encoding="utf-8")
+        for item in stream:
+            try:
+                data_out = json.dumps(item, indent=indent, sort_keys=True, ensure_ascii=False)
+            except Exception as exc:
+                data_out = "# (json failed: %r)  # %r" % (exc, item)
+            sys.stdout.write(data_out)
+            sys.stdout.write("\n")
+            sys.stdout.flush()
     except Exception as exc:
         sys.stderr.write("#  -!!---- %s\n" % (exc,))
-        sys.stdout.write(repr(data_in))
+        sys.stdout.write(repr(data_in.read()))
         return 13
-
-    data_out = json.dumps(data, indent=indent, sort_keys=True, ensure_ascii=False)
-    sys.stdout.write(to_bytes(data_out))
-    sys.stdout.write("\n")
 
 
 if __name__ == '__main__':
