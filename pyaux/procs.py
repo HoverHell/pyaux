6244f88b4332bc29093d0187a702493919406d91
HoverHell <hoverhell@gmail.com>
2018-11-26 17:37:02 +0300

    procs: py2 support fix

--- pyaux/procs.py
+++ pyaux/procs.py
@@ -111,13 +111,12 @@ def poll_fds(
             yield tag, timestamp, data
 
 
-def run_cmd(
-        *args,
-        timeout=600,
-        stdout_cb=stdout_cb_default,
-        stderr_cb=stderr_cb_default,
-        nonblocking=False,
-        **kwargs):
+def run_cmd(*args, **kwargs):
+
+    timeout = kwargs.pop('timeout', 600)
+    stdout_cb = kwargs.pop('stdout_cb', stdout_cb_default)
+    stderr_cb = kwargs.pop('stderr_cb', stderr_cb_default)
+    nonblocking = kwargs.pop('nonblocking', False)
 
     tag_to_cb = dict(stdout=stdout_cb, stderr=stderr_cb)
 
